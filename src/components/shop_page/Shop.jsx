a7d6c2188eb5f6a90e16f6eddf1e49e6e4b1a5e2
J <0xjariel@protonmail.com>
2023-08-17 19:27:56 -0600

    shop page carousel

--- src/components/shop_page/Shop.jsx
+++ src/components/shop_page/Shop.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { StarIcon } from "@heroicons/react/20/solid";
+import { Link } from "react-router-dom";
+import ShopCarousel from "../shop_page_carousel/ShopCarousel";
+import ShopBody from "../shop_body/ShopBody";
 
 /*
   This example requires some changes to your config:
@@ -139,30 +142,13 @@ const products = [
   // More products...
 ];
 
-export default function Shop() {
+function Shop() {
   return (
-    <div className="bg-white">
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
-
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-          {products.map((product) => (
-            <a key={product.id} href={product.href} className="group">
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                <img
-                  src={product.imageSrc}
-                  alt={product.imageAlt}
-                  className="h-full w-full object-cover object-center group-hover:opacity-75"
-                />
-              </div>
-              <h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
-              <p className="mt-1 text-lg font-medium text-gray-900">
-                {product.price}
-              </p>
-            </a>
-          ))}
-        </div>
-      </div>
-    </div>
+    <>
+      <ShopBody products={products} />
+      <ShopCarousel />
+    </>
   );
 }
+
+export default Shop;
--- src/components/shop_page_carousel/ShopCarousel.jsx
+++ src/components/shop_page_carousel/ShopCarousel.jsx
@@ -1,3 +1,22 @@
+import { useState } from "react";
+import { FaChevronCircleLeft, FaChevronCircleRight } from "react-icons/fa";
+
 export default function ShopCarousel() {
-  return <div></div>;
+  let [pageNumber, setPageNumber] = useState(1);
+
+  const goToNextPage = () => {
+    setPageNumber(pageNumber + 1);
+  };
+
+  const goToPreviousPage = () => {
+    setPageNumber(pageNumber - 1);
+  };
+
+  return (
+    <div className="flex mx-auto gap-4 text-4xl">
+      <FaChevronCircleLeft onClick={goToPreviousPage} />
+      {pageNumber}
+      <FaChevronCircleRight onClick={goToNextPage} />
+    </div>
+  );
 }
