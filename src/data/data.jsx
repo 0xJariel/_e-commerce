ceff7c625b9cadc0736c15db604c711be0c87414
J <0xjariel@protonmail.com>
2023-10-02 18:23:43 -0600

    fixing for deployement

--- src/data/data.jsx
+++ src/data/data.jsx
@@ -67,57 +67,57 @@ function getData(filter) {
         // More sections...
       ],
     },
-    {
-      id: uuidv4(),
-      name: "Large Bowl",
-      type: "bowl",
-      href: "#",
-      price: 22,
-      imageSrc: largeBowl,
-      imageAlt:
-        "Olive drab green insulated bottle with flared screw lid and flat top.",
-      rating: 4,
-      images: [
-        {
-          id: 1,
-          name: "Angled view",
-          src: largeBowl,
-          alt: "Angled front view with bag zipped and handles upright.",
-        },
-        // More images...
-      ],
-      colors: [
-        {
-          name: "Washed Black",
-          bgColor: "bg-gray-700",
-          selectedColor: "ring-gray-700",
-        },
-        { name: "White", bgColor: "bg-white", selectedColor: "ring-gray-400" },
-        {
-          name: "Washed Gray",
-          bgColor: "bg-gray-500",
-          selectedColor: "ring-gray-500",
-        },
-      ],
-      description: `
-          The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.
-        `,
-      details: [
-        {
-          name: "Features",
-          items: [
-            "Multiple strap configurations",
-            "Spacious interior with top zip",
-            "Leather handle and tabs",
-            "Interior dividers",
-            "Stainless strap loops",
-            "Double stitched construction",
-            "Water-resistant",
-          ],
-        },
-        // More sections...
-      ],
-    },
+    // {
+    //   id: uuidv4(),
+    //   name: "Large Bowl",
+    //   type: "bowl",
+    //   href: "#",
+    //   price: 22,
+    //   imageSrc: largeBowl,
+    //   imageAlt:
+    //     "Olive drab green insulated bottle with flared screw lid and flat top.",
+    //   rating: 4,
+    //   images: [
+    //     {
+    //       id: 1,
+    //       name: "Angled view",
+    //       src: largeBowl,
+    //       alt: "Angled front view with bag zipped and handles upright.",
+    //     },
+    //     // More images...
+    //   ],
+    //   colors: [
+    //     {
+    //       name: "Washed Black",
+    //       bgColor: "bg-gray-700",
+    //       selectedColor: "ring-gray-700",
+    //     },
+    //     { name: "White", bgColor: "bg-white", selectedColor: "ring-gray-400" },
+    //     {
+    //       name: "Washed Gray",
+    //       bgColor: "bg-gray-500",
+    //       selectedColor: "ring-gray-500",
+    //     },
+    //   ],
+    //   description: `
+    //       The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.
+    //     `,
+    //   details: [
+    //     {
+    //       name: "Features",
+    //       items: [
+    //         "Multiple strap configurations",
+    //         "Spacious interior with top zip",
+    //         "Leather handle and tabs",
+    //         "Interior dividers",
+    //         "Stainless strap loops",
+    //         "Double stitched construction",
+    //         "Water-resistant",
+    //       ],
+    //     },
+    //     // More sections...
+    //   ],
+    // },
     {
       id: uuidv4(),
       name: "Porcelin Mug",
